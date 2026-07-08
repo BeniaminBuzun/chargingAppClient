39e99e39e1db83fce7779dad6d0b9c7701a3cacf
BeniaminBuzun <benekbuz@gmail.com>
2026-07-08 23:21:15 +0200

    changed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}', // <-- ADD THIS LINE
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}', // <-- ADD THIS LINE
     
     // Note: If you used a 'src' directory when initializing Next.js, 
     // it should be: './src/components/**/*.{js,ts,jsx,tsx,mdx}'
